432c3c2bd3f109967c5ce53a2731a5e4a09efb69
D8GER <xyb5to0zcy@gmail.com>
2025-07-01 01:21:03 +0800

    refactor(update): update windrun-huaiin, and now can use it's full features!

--- .source/index.ts
+++ .source/index.ts
@@ -1,8 +1,6 @@
 // @ts-nocheck -- skip type checking
-import * as legal_1 from "../src/mdx/legal/terms.mdx?collection=legal&hash=1751285930820"
-import * as legal_0 from "../src/mdx/legal/privacy.mdx?collection=legal&hash=1751285930820"
+import * as legal_1 from "../src/mdx/legal/terms.mdx?collection=legal&hash=1751296311890"
+import * as legal_0 from "../src/mdx/legal/privacy.mdx?collection=legal&hash=1751296311890"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const blog = _runtime.docs<typeof _source.blog>([], [])
-export const docs = _runtime.docs<typeof _source.docs>([], [])
 export const legal = _runtime.docs<typeof _source.legal>([{ info: {"path":"privacy.mdx","absolutePath":"/Users/funeye/IdeaProjects/reve-image-landing/src/mdx/legal/privacy.mdx"}, data: legal_0 }, { info: {"path":"terms.mdx","absolutePath":"/Users/funeye/IdeaProjects/reve-image-landing/src/mdx/legal/terms.mdx"}, data: legal_1 }], [{"info":{"path":"meta.json","absolutePath":"/Users/funeye/IdeaProjects/reve-image-landing/src/mdx/legal/meta.json"},"data":{"pages":["terms","privacy"]}}])
